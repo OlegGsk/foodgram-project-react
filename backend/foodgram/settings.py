ffd36b05ab7090e9cf53528003981aafedc1b220
Олег Говоровский <oleq.gov@ya.ru>
2024-03-25 10:29:57 +0500

    fix docker-compose

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', default=True)
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='*').split()
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(', ')
 
 
 # Application definition
